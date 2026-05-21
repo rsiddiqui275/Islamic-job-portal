d1e5f64dbc63c7a4d1d5aff11bda3f781301bf71
REhan <rsiddiqui@gmail.com>
2026-05-21 12:54:29 +0530

    Force rebuild with updated env

--- MolanaApp.Web/src/environments/environment.ts
+++ MolanaApp.Web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-   apiUrl: 'https://islamic-jobs-api.onrender.com/api'
-
+  // Force rebuild: 2026-05-21
+  apiUrl: 'https://islamic-jobs-api.onrender.com/api'
 };
